0587bfb06f3b9281aa7ad4076f946ff180eeb7b6
Joselst0108 <joselst0108@gmail.com>
2026-02-03 18:12:17 -0500

    Actualizar supabaseClient.js

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,15 +1,15 @@
 // assets/js/supabaseClient.js
 
-const supabaseUrl = "https://rvdafufkhyjtauubirkz.supabase.co";
-const supabaseAnonKey =
+const SUPABASE_URL = "https://rvdafufkhyjtauubirkz.supabase.co";
+const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2ZGFmdWZraHlqdGF1dWJpcmt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNzM5MDQsImV4cCI6MjA4NTY0OTkwNH0.Yo0EC8g9v0DHebiFyS445EbLMYHw14U2x3VN1_ZmKAk";
 
-if (typeof supabase === "undefined") {
+if (typeof window.supabase === "undefined") {
   console.error("❌ Supabase CDN no cargado");
 } else {
-  window.supabaseClient = supabase.createClient(
-    supabaseUrl,
-    supabaseAnonKey
+  window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
   );
-  console.log("✅ supabaseClient listo");
+  console.log("✅ Supabase inicializado correctamente");
 }
