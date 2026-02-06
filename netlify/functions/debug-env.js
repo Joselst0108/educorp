36bec4cfc2609ec13959c71f0ce535eb304c1342
Joselst0108 <joselst0108@gmail.com>
2026-02-06 00:07:22 -0500

    Actualizar debug-env.js

--- netlify/functions/debug-env.js
+++ netlify/functions/debug-env.js
@@ -1,12 +1,12 @@
 exports.handler = async () => {
+  const key = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
   return {
     statusCode: 200,
     body: JSON.stringify({
       hasUrl: !!process.env.SUPABASE_URL,
-      hasKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-      keyLength: process.env.SUPABASE_SERVICE_ROLE_KEY
-        ? process.env.SUPABASE_SERVICE_ROLE_KEY.length
-        : 0
+      hasKey: !!key,
+      keyPrefix: key.slice(0, 10), // ej: "sb_secret_" o "eyJhbGciO"
+      urlSample: (process.env.SUPABASE_URL || "").slice(0, 30)
     })
   };
 };
