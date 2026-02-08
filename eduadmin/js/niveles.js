b4ad787391be0802b1a2b25e71f910fcfbb25f97
Joselst0108 <joselst0108@gmail.com>
2026-02-07 21:39:02 -0500

    Actualizar niveles.js

--- eduadmin/js/niveles.js
+++ eduadmin/js/niveles.js
@@ -1,3 +1,11 @@
+async function getUserData() {
+    const { data, error } = await supabase.auth.getUser();
+    if (error) {
+        console.error(error);
+        return null;
+    }
+    return data.user;
+}
 // /eduadmin/js/niveles.js
 (() => {
   const supabase = () => window.supabaseClient;
